74912440f5a8df264b7703ebce4f138f5c9b96ed
Krish Moran <moran.kri@husky.neu.edu>
2018-11-17 10:16:19 -0500

    data def stuff

--- PS17.rkt
+++ PS17.rkt
@@ -8,10 +8,29 @@
 ; A Person is a (make-person String Belief [List-of String])
 (define-struct person [name belief friends])
 ; and represents their name, their belief, and the name of their friends
+; Examples:
+(define PERSON-1 (make-person "Alice" "red" (list "Carol" "Heidi")))
+(define PERSON-2 (make-person "Bob" "blue" (list "Carol" "Dan")))
+(define PERSON-3 (make-person "Carol" "red" (list)))
+(define PERSON-4 (make-person "Dan" "blue" (list "Carol" "Eric" "Frank" "Grace")))
+(define PERSON-5 (make-person "Eric" "red" (list "Alice" "Bob" "Carol" "Dan" "Frank" "Grace")))
+(define PERSON-6  (make-person "Frank" "blue" (list "Alice" "Bob" "Carol" "Dan" "Grace")))
+; Template:
+#;
+(define (person-temp p)
+  ... (person-name p) ... (belief-temp (person-belief p)) ... (person-friends p))
+ 
  
 ; A Belief is one of:
 ; - "blue"
 ; - "red"
+;Template:
+#;
+(define (belief-temp b)
+  (cond
+    [(string=? b "blue") ...]
+    [(string=? b "red") ...]))
+                                                 
  
 (define NETWORK
   (list
@@ -29,3 +48,9 @@
 ; update-network : Network -> Network
 ; updates every individual's belief to become the belief the majority of their friends have
 ; if there is a tie, then the belief is unchanged
+
+; Exercise 2
+
+; can-reach? : String String Network -> Boolean
+; takes the names of two people and a network, asks if the first person can reach the second
+;;;;;NOTE: Anyone can reach themselves, and anyone can reach another person through their friends if that friend has the same belief as them.
