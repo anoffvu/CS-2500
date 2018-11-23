3d9a687153da8936d28759f5e7cb6ce53765432c
anoffvu <anoffvu@gmail.com>
2018-11-22 19:53:37 -0600

    finished and added check expects

--- Pset 18/Pset 18.rkt
+++ Pset 18/Pset 18.rkt
@@ -17,7 +17,12 @@
 ; Termination: every time we recur, we get rid of the last digit, which will eventually
 ; find itself within our trivial case of [0,9] for any non-negative integer
 ; then we can just use digit->string to transform it to a string
-; CEs NEEDED
+
+(check-expect (nat->string 123456789) "123456789")
+(check-expect (nat->string 12345678) "12345678")
+(check-expect (nat->string 1) "1")
+(check-expect (nat->string 10) "10")
+
 (define (nat->string nni)
   (local [(define LAST-DIGIT (digit->string (modulo nni 10)))
           (define ALL-OTHER-DIGITS (grab-all-other-digits nni))]
@@ -26,19 +31,18 @@
       [(> nni 9) (string-append ALL-OTHER-DIGITS LAST-DIGIT)])))
 
 ; grab-all-other-digits : NonNegative
-; grabs all but the last digits in a number
-; CEs NEEDED
+; grabs all but the last digit in a number and turns it into a string
+(check-expect (grab-all-other-digits 123456789) "12345678")
+(check-expect (grab-all-other-digits 12345678) "1234567")
+(check-expect (grab-all-other-digits 1) "")
+(check-expect (grab-all-other-digits 10) "1")
+
 (define (grab-all-other-digits nni)
   (cond
     [(<= 0 nni 9) ""]
     [(> nni 9) (nat->string (floor (/ nni 10)))]))
 
 ;ex 2
-
-; Exercise 2 Design the function string-split,
-; which given a string to split and a non-empty delimiter splits the string by that delimiter.
-; For example, (string-split "hello%%bob%%%i%%%%am%%jack" "%%")
-; should return (list "hello" "bob" "%i" "" "am" "jack").
 
 ; string-split : String String -> [List-of String]
 ; Given a string and a delimiter (second string
@@ -47,23 +51,44 @@
 ; case of the string not containing any instances of the delimiter, in which case we return the rest
 ; of the string
 
+(check-expect (string-split "hello o o o ll o hillo" "ll") (list "he" "o o o o " " o hi" "o"))
+(check-expect (string-split "hello o o o ll o hillo" "not-in-string") (list "hello o o o ll o hillo"))
+(check-expect (string-split "1212121212 12 12 1" "1") (list "" "2" "2" "2" "2" "2 " "2 " "2 " ""))
+(check-expect (string-split "1 1 1 1 1 1 1    1" " ") (list "1" "1" "1" "1" "1" "1" "1" "" "" "" "1"))
+(check-expect (string-split "hello%%bob%%%i%%%%am%%jack" "%%")
+              (list "hello" "bob" "%i" "" "am" "jack"))
+(check-expect (string-split "111111114" "1") (list "" "" "" "" "" "" "" "" "4"))
+(check-expect (string-split "111111114" "4") (list "11111111" ""))
+
 (define (string-split longstring delimiter)
-  (local [(define DELIMITER-LENGTH (length delimiter))
-          (define EXPLODED-DELIMITER (explode delmiter))]
-    (cond
-      [(not (string-contains? delimiter longstring)) (list longstring)]
-      [(string-contains? delimiter longstring)
-       (cons (first (cut-first-segment longstring delimiter))
-             (string-split (second (cut-first-segment longstring delimiter))))])))
+    (if
+      (not (string-contains? delimiter longstring))
+      (list longstring)
+      (cons (first (cut-first-segment longstring delimiter 0))
+            (string-split (second (cut-first-segment longstring delimiter 0)) delimiter))))
 
-; cut-first-segment : String String -> [List-of
+; cut-first-segment : String String NonNegativeInteger -> [List-of String]
+; Cuts the string into 2 parts: the string before the delimiter and the string after the delimiter
+; the string must have a delimiter in it as that is the only case where we call this function
 
-; dont wanna do list of string because consing then will give a fuck ton of list
-; might do individual helpers to just grab the first segment and then grab the rest of the segment
+(check-expect (cut-first-segment "hello" "ll" 0) (list "he" "o"))
+(check-expect (cut-first-segment "hello" "o" 0) (list "hell" ""))
+(check-expect (cut-first-segment "hellollo" "ll" 0) (list "he" "ollo"))
+(check-expect (cut-first-segment "llllllllll" "ll" 0) (list "" "llllllll"))
+(check-expect (cut-first-segment "ollllllllll" "ll" 0) (list "o" "llllllll"))
 
-    
-  
-
-
-
-
+(define (cut-first-segment longstring delimiter look-index)
+  (local [(define DELIMITER-LENGTH (string-length delimiter))
+          ; grab-segment-at-index : NonNegativeInteger -> String
+          ; grabs a string from the longstring that is the same size as the delimiter
+          ; at the given index
+          ; for the checkexpects, assume the longstring is "hello123" and the delimiter is "00"
+          ; given 3, returns "lo"
+          ; given 0, returns "he"
+          ; given 6, returns "23"
+          (define (grab-segment-at-index index)
+            (substring longstring index (+ index DELIMITER-LENGTH)))]
+    (if (string=? (grab-segment-at-index look-index) delimiter)
+        (list (substring longstring 0 look-index)
+              (substring longstring (+ look-index DELIMITER-LENGTH) (string-length longstring)))
+        (cut-first-segment longstring delimiter (+ look-index 1)))))
